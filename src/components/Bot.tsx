95b718cdff66c8c9e9075bec21242e5812c9991f
nick-choudhary <nchoudhary110792@gmail.com>
2023-08-31 17:02:23 -0500

    update-branding

--- src/components/Bot.tsx
+++ src/components/Bot.tsx
@@ -390,7 +390,8 @@ export const Bot = (props: BotProps & { class?: string }) => {
                         defaultValue={userInput()}
                         onSubmit={handleSubmit}
                     />
-                </div>
+                </div
+                <Badge badgeBackgroundColor={props.badgeBackgroundColor} poweredByTextColor={props.poweredByTextColor} botContainer={botContainer} />
                 <BottomSpacer ref={bottomSpacer} />
             </div>
             {sourcePopupOpen() && <Popup isOpen={sourcePopupOpen()} value={sourcePopupSrc()} onClose={() => setSourcePopupOpen(false)} />}
